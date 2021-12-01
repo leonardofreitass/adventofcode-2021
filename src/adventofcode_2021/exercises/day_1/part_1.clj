17fd602f6c8c2ca85d726802ac12337071c4df7b
Leonardo Freitas dos Santos <leonardo.freitas.s@outlook.com>
2021-12-01 09:56:14 +0100

    feat: day 1 part 1

--- src/adventofcode_2021/exercises/day_1/part_1.clj
+++ src/adventofcode_2021/exercises/day_1/part_1.clj
@@ -2,4 +2,10 @@
 
 (defn run
   [inputs]
-  (println "Hi"))
+  ((reduce
+      (fn [{:keys [last acc]} next]
+        (if (and (not (nil? last)) (> next last))
+          {:last next :acc (inc acc)}
+          {:last next :acc acc}))
+      {:last nil :acc 0}
+      (map #(Integer/parseInt %) inputs)) :acc))
