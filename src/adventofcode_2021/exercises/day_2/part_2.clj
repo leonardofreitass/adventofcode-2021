33a2c124b8032f49ddd08253bd1fe2f93b2ab380
Leonardo Freitas dos Santos <leonardo.freitas.s@outlook.com>
2021-12-02 09:19:16 +0100

    feat: day 2 part 2

--- src/adventofcode_2021/exercises/day_2/part_2.clj
+++ src/adventofcode_2021/exercises/day_2/part_2.clj
@@ -1,16 +1,16 @@
-(ns adventofcode-2021.exercises.day-2.part-1)
+(ns adventofcode-2021.exercises.day-2.part-2)
 
 (require '[clojure.string :as str])
 
 (defn exec-command
-  [[pos depth] line]
+  [[pos depth aim] line]
   (let [[cmd n] (str/split line #" ")
         number (Integer/parseInt n)]
     (case cmd
-      "forward" [(+ pos number) depth]
-      "down" [pos (+ depth number)]
-      "up" [pos (- depth number)])))
+      "forward" [(+ pos number) (+ depth (* aim number)) aim]
+      "down" [pos depth (+ aim number)]
+      "up" [pos depth (- aim number)])))
 
 (defn run
   [inputs]
-  (apply * (reduce exec-command [0 0] inputs)))
+  (apply * (pop (reduce exec-command [0 0 0] inputs))))
