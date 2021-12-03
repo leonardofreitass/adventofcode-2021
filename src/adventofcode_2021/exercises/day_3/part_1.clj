f0e930a4f320d20b66be8184467a7f761e1a7ef8
Leonardo Freitas dos Santos <leonardo.freitas.s@outlook.com>
2021-12-03 11:43:52 +0100

    feat: day 3 part 1

---
+++ src/adventofcode_2021/exercises/day_3/part_1.clj
@@ -0,0 +1,23 @@
+(ns adventofcode-2021.exercises.day-3.part-1)
+
+(require '[clojure.string :as str])
+
+(defn transpose
+  [inputs]
+  (apply map list (map #(str/split % #"") inputs)))
+
+(defn power-consumption
+  [readings]
+  (apply * (map #(Integer/parseInt % 2) readings)))
+
+(defn run
+  [inputs]
+  (power-consumption
+    (reduce
+      (fn [[gamma epsilon] line]
+        (let [freq (frequencies line)
+              g (key (apply max-key val freq))
+              e (key (apply min-key val freq))]
+          [(str gamma g) (str epsilon e)]))
+      ["" ""]
+      (transpose inputs))))
